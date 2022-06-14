77aecbc41d9bb4d9da1e93657d554f6587edcfc4
Thorn324 <lychanthorn2000@gmail.com>
2022-06-14 10:33:56 +0700

    update

--- controllers/cityController.js
+++ controllers/cityController.js
@@ -1,60 +1,63 @@
 const City = require("../models/City");
 
-exports.get_all = async (req, res) => {
-  try {
-    const all = await City.find();
-    res.status(200).json({ success: true, data: all });
-  } catch (error) {
-    res.status(500).json({ success: false, error: error });
-  }
+exports.get_all = async(req, res) => {
+    try {
+        const all = await City.find();
+        res.status(200).json({ success: true, data: all });
+    } catch (error) {
+        res.status(500).json({ success: false, error: error });
+    }
 };
 
-exports.by_id = async (req, res) => {
-  const { id } = req.params;
+exports.by_id = async(req, res) => {
+    const { id } = req.params;
 
-  try {
-    const doc = await City.findById(id);
-    res.status(200).json({ success: true, data: doc });
-  } catch (error) {
-    res.status(400).json({ success: false, error: error });
-  }
+    try {
+        const doc = await City.findById(id);
+        res.status(200).json({ success: true, data: doc });
+    } catch (error) {
+        res.status(400).json({ success: false, error: error });
+    }
 };
 
-exports.create_new = async (req, res) => {
-  try {
-    const doc = await City.create(req.body);
-    await doc.save();
+exports.create_new = async(req, res) => {
+    try {
+        const doc = await City.create(req.body);
+        await doc.save();
 
-    res.status(200).json({ success: true, data: doc });
-  } catch (error) {
-    res.status(400).json({ success: false, error: error });
-  }
+        res.status(200).json({ success: true, data: doc });
+    } catch (error) {
+        res.status(400).json({ success: false, error: error });
+    }
 };
 
-exports.update_by_id = async (req, res) => {
-  const { id } = req.params;
-  const { name, description, thumbnail } = req.body;
+exports.update_by_id = async(req, res) => {
+    const { id } = req.params;
+    const { name, description, thumbnail, lat, lng, image } = req.body;
 
-  try {
-    const doc = await City.findById(id);
-  
-    if (name) doc.name = name;
-    if (description) doc.description = description;
-    if (thumbnail) doc.thumbnail = thumbnail;
+    try {
+        const doc = await City.findById(id);
 
-    await doc.save();
-    res.status(200).json({ success: true, data: doc });
-  } catch (error) {
-    res.status(400).json({ success: false, error: error });
-  }
+        if (name) doc.name = name;
+        if (description) doc.description = description;
+        if (thumbnail) doc.thumbnail = thumbnail;
+        if (lat) doc.lat = lat;
+        if (lng) doc.lng = lng;
+        if (image) doc.image = image;
+
+        await doc.save();
+        res.status(200).json({ success: true, data: doc });
+    } catch (error) {
+        res.status(400).json({ success: false, error: error });
+    }
 };
 
-exports.delete_by_id = async (req, res) => {
-  const { id } = req.params;
-  try {
-    const deleting = await City.deleteOne({ _id: id });
-    res.status(200).json({ success: true, data: deleting });
-  } catch (error) {
-    res.status(400).json({ success: false, error: error });
-  }
+exports.delete_by_id = async(req, res) => {
+    const { id } = req.params;
+    try {
+        const deleting = await City.deleteOne({ _id: id });
+        res.status(200).json({ success: true, data: deleting });
+    } catch (error) {
+        res.status(400).json({ success: false, error: error });
+    }
 };
--- models/City.js
+++ models/City.js
@@ -1,16 +1,25 @@
 const mongoose = require("mongoose");
 
 const citySchema = mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-  },
-  description: {
-    type: String,
-  },
-  thumbnail: {
-    type: String,
-  },
+    name: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+    },
+    thumbnail: {
+        type: [String],
+    },
+    image: {
+        type: String,
+    },
+    lat: {
+        type: String,
+    },
+    lng: {
+        type: String,
+    },
 });
 
 module.exports = mongoose.model("City", citySchema);
