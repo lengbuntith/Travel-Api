24a1a2330276c95f16c6dc0a68ccb39dea7cd2e9
Thorn324 <lychanthorn2000@gmail.com>
2022-06-15 22:07:40 +0700

    add filter comment

--- models/User.js
+++ models/User.js
@@ -3,115 +3,112 @@ const bcrypt = require("bcrypt");
 const validator = require("validator");
 const crypto = require("crypto");
 
-const userSchema = new mongoose.Schema(
-  {
+const userSchema = new mongoose.Schema({
     email: {
-      type: String,
-      required: [true, "Please enter an email"],
-      unique: true,
-      validate: [validator.isEmail, "Please enter a valid email"],
+        type: String,
+        required: [true, "Please enter an email"],
+        unique: true,
+        validate: [validator.isEmail, "Please enter a valid email"],
     },
     firstName: {
-      type: String,
-      required: [true, "Please enter your first name"],
+        type: String,
+        required: [true, "Please enter your first name"],
     },
     lastName: {
-      type: String,
-      required: [true, "Please enter your last name"],
+        type: String,
+        required: [true, "Please enter your last name"],
     },
     password: {
-      type: String,
-      required: [true, "Please enter a password"],
-      minlength: [8, "Minimum password is 8 characters"],
+        type: String,
+        required: [true, "Please enter a password"],
+        minlength: [8, "Minimum password is 8 characters"],
     },
     confirmPassword: {
-      type: String,
-      validate: {
-        //only work on CREATE and SAVE
-        validator: function (el) {
-          return el === this.password;
+        type: String,
+        validate: {
+            //only work on CREATE and SAVE
+            validator: function(el) {
+                return el === this.password;
+            },
+            message: "Password are not the same",
         },
-        message: "Password are not the same",
-      },
     },
     role: {
-      type: String,
-      default: "user",
-      validate: {
-        validator: function (el) {
-          if (el != "user") return false;
+        type: String,
+        default: "user",
+        validate: {
+            validator: function(el) {
+                if (el != "user") return false;
+            },
+            message: "You not allow to define user role",
         },
-        message: "You not allow to define user role",
-      },
     },
     imageUrl: {
-      type: String,
+        type: String,
     },
     passwordResetToken: {
-      type: String,
+        type: String,
     },
     passwordResetExpires: {
-      type: Date,
+        type: Date,
     },
-  },
-  { timestamps: true }
-);
+}, { timestamps: true });
 
 //before save document
-userSchema.pre("save", async function (next) {
-  //if not modified password do nothing
-  if (!this.isModified("password")) return next();
+userSchema.pre("save", async function(next) {
+    //if not modified password do nothing
+    if (!this.isModified("password")) return next();
 
-  //hash password
-  const salt = await bcrypt.genSalt();
-  this.password = await bcrypt.hash(this.password, salt);
+    //hash password
+    const salt = await bcrypt.genSalt();
+    this.password = await bcrypt.hash(this.password, salt);
 
-  //delete password confirm field
-  this.confirmPassword = undefined;
+    //delete password confirm field
+    this.confirmPassword = undefined;
 
-  next();
+    next();
 });
 
 // static method to login user
-userSchema.statics.login = async function (email, password) {
-  try {
-    const user = await this.findOne({ email });
-    if (user) {
-      const auth = await bcrypt.compare(password, user.password);
-      if (auth) {
-        return user;
-      }
-      throw Error("incorrect password");
-    }
-    throw Error("incorrect email");
-  } catch (error) {
-    let errors = "";
-    console.log("User.login", error.message);
+userSchema.statics.login = async function(email, password) {
+    try {
+        const user = await this.findOne({ email });
+        if (user) {
+            const auth = await bcrypt.compare(password, user.password);
+            if (auth) {
+                return user;
+            }
+            throw Error("incorrect password");
+        }
+        throw Error("incorrect email");
+    } catch (error) {
+        let errors = "";
+        console.log("User.login", error.message);
 
-    if (error.message.includes("data and hash arguments required")) {
-      errors = "Please enter password";
-    } else {
-      errors = error;
+        if (error.message.includes("data and hash arguments required")) {
+            errors = "Please enter password";
+        } else {
+            errors = error;
+        }
+        throw Error(errors);
     }
-    throw Error(errors);
-  }
 };
 
 //create reset token
-userSchema.methods.createPasswordResetToken = function () {
-  const resetToken = crypto.randomBytes(32).toString("hex");
+userSchema.methods.createPasswordResetToken = function() {
+    const resetToken = crypto.randomBytes(32).toString("hex");
 
-  this.passwordResetToken = crypto
-    .createHash("sha256")
-    .update(resetToken)
-    .digest("hex");
+    this.passwordResetToken = crypto
+        .createHash("sha256")
+        .update(resetToken)
+        .digest("hex");
 
-  console.log({ resetToken }, this.passwordResetToken);
+    console.log({ resetToken }, this.passwordResetToken);
 
-  this.passwordResetExpires = Date.now() + 10 * 60 * 1000;
+    this.passwordResetExpires = Date.now() + 10 * 60 * 1000;
 
-  return resetToken;
+    return resetToken;
 };
 
-const User = mongoose.model("User", userSchema);
-module.exports = User;
+module.exports = mongoose.model("User", userSchema);
+// module.exports = User;
--- routes/cityRoute.js
+++ routes/cityRoute.js
@@ -7,7 +7,7 @@ const router = Router();
 router.post("/create", requireAuthAdmin, cityController.create_new);
 router.get("/all", cityController.get_all);
 router.get("/detail/:id", cityController.by_id);
-router.post("/update/:id", cityController.update_by_id);
+router.post("/update/:id", requireAuthAdmin, cityController.update_by_id);
 router.post("/delete/:id", requireAuthAdmin, cityController.delete_by_id);
 
 module.exports = router;
